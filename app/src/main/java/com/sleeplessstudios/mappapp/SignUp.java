0337743f25938542a651742821c620656b013913
Ilse-May Brink <18023892@vegaconnect.co.za>
2021-11-09 13:28:12 +0200

    class name changes

--- app/src/main/java/com/sleeplessstudios/mappapp/SignUp.java
+++ app/src/main/java/com/sleeplessstudios/mappapp/SignUp.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class signup extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
