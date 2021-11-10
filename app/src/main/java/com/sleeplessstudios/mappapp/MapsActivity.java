d28ab73473d839d569ee2e982b1390eb81543956
Jody <18020741@vegaconnect.co.za>
2021-11-10 12:10:10 +0200

    map v3

--- app/src/main/java/com/sleeplessstudios/mappapp/MapsActivity.java
+++ app/src/main/java/com/sleeplessstudios/mappapp/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng capeTown = new LatLng(-33.9249, 18.4241);
+        //mMap.addMarker(new MarkerOptions().position(capeTown).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(capeTown, 15f));
     }
 }
