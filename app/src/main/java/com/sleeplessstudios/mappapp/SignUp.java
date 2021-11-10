61284cc1050ace88c962a1d9fb60435b1f90c806
Ilse-May Brink <18023892@vegaconnect.co.za>
2021-11-10 12:58:34 +0200

    signup buttons

--- app/src/main/java/com/sleeplessstudios/mappapp/SignUp.java
+++ app/src/main/java/com/sleeplessstudios/mappapp/SignUp.java
@@ -2,21 +2,29 @@ package com.sleeplessstudios.mappapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUp extends AppCompatActivity {
     private ImageButton signUp;
+    private EditText username;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        //button listeners
         signUp = findViewById(R.id.signup_btn);
         signUp.setOnClickListener(v -> openMapScreen());
 
+        username = findViewById(R.id.signup_username_txt);
+        email = findViewById(R.id.signup_email_txt);
+        password = findViewById(R.id.signup_password_txt);
     }
 
     public void openMapScreen()
